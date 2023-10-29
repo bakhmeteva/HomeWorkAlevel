11fb4c6dbc1aeb0b16fccea93b3b7274b59b43c3
Bakhmeteva Yulya <bakhmetevayulya@gmail.com>
2023-10-29 17:04:54 +0200

    LessModuleNumber

---
+++ src/lesson2/LessModuleNumber.java
@@ -0,0 +1,22 @@
+package lesson2;
+
+public class LessModuleNumber {
+    public static void main(String[] args) {
+        int n1 = 12;
+        int n2 = -7;
+        int n3 = 3;
+
+        int absN1 = Math.abs(n1);
+        int absN2 = Math.abs(n2);
+        int absN3 = Math.abs(n3);
+
+        if (absN1 <= absN2 && absN1 <= absN3) {
+            System.out.println("Число с наименьшим модулем: " + n1);
+        } else if (absN2 <= absN1 && absN2 <= absN3) {
+            System.out.println("Число с наименьшим модулем: " + n2);
+        } else {
+            System.out.println("Число с наименьшим модулем: " + n3);
+        }
+
+    }
+}
